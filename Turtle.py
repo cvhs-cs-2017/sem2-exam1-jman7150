b8e06391df8513e33dcf4d03fc0453731328b970
Justin Smith <smithju071500@cvh.rusd.us>
2017-02-01 11:46:22 -0800

    part 2 of turtles

--- Turtle.py
+++ Turtle.py
@@ -3,6 +3,14 @@
 
 
 
-
 """Import and Call the DrawRectangle(Anyturtle, l, w) function from the
 file MyFile.py"""
+
+def DrawRectangle(exam, l, w):
+    for i in range (4):
+        l = 14
+        w = 20
+        exam.forward(l)
+        exam.right(90)
+        exam.forward(w)
+DrawRectangle(exam, l , w)
