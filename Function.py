4a86c32f4e0fa200eb2981129a4dc0844840d946
Justin Smith <smithju071500@cvh.rusd.us>
2017-02-03 10:01:20 -0800

    completed part 2 of function

--- Function.py
+++ Function.py
@@ -7,6 +7,10 @@ print(triple(12))
 
 
 
+
 """Write a program that will prompt the user for an input value (n) and print
 the result of 3n by calling the function defined above.  Make sure you include
 the necessary print statements and address any issues with whitespace. """
+print("Input a value")
+n = int(input())
+print(triple(n))
