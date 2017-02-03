7bdbc8f7c71e00948cb0eb6002d893f7d9b7179d
Justin Smith <smithju071500@cvh.rusd.us>
2017-02-03 10:39:01 -0800

    finished loops

--- Loops.py
+++ Loops.py
@@ -1,6 +1,17 @@
 """Write a program that will add 5 and loop until it reaches a number GREATER
 than 100.  It should then spit out the result AND tell the user how many times
 it had to add 5 (if any)"""
+for i in range(100):
+    def loops():
+        b = 5
+        if b < 100:
+            b = b + 5
+        elif b > 100:
+            b = b + 0
+        return b
+print(loops)
+
+
 
 
 
@@ -10,3 +21,13 @@ it had to add 5 (if any)"""
 """Write a program that will prompt the user for an input value (n) and double
 it IF is an ODD number, triple it if is an EVEN number and do nothing if it is
 anything else (like a decimal or a string)"""
+
+def loops():
+    print("Input value")
+    b = int(input())
+    if b = even:
+        b = b * 3
+    elif b = odd:
+        b = b * 2
+    return b
+print(loops)
